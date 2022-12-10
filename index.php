6e33982851e4d02dd505d6475339bd23481d5a0c
Gustavo Ando <gustavo.k.ando@gmail.com>
2022-12-10 11:27:51 -0300

    criando tabela de usuarios na pagina home

--- index.php
+++ index.php
@@ -1,11 +1,54 @@
+<?php
+    include("./database/conexao.php");
+    $sql_code = "SELECT * FROM usuarios";
+    $sql_query = $conn->query($sql_code) or die($mysqli->error);
+    $row = $sql_query->fetch_assoc();
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World!</title>
+    <title>Usuários</title>
+    <link rel="stylesheet" href="style.css?v=<? echo time() ?>">
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <h1>Controle de Usuários</h1>
+<?php
+    if (!empty($row)) {
+?>
+    <table>
+        <tr>
+            <td>Nome</td>
+            <td>E-mail</td>
+            <td>Projeto</td>
+            <td colspan="3">Ação</td>
+        </tr>
+<?php
+    do{
+?>
+        <tr>
+            <td><?php echo $row['nome'] ?></td>
+            <td><?php echo $row['email'] ?></td>
+            <td><?php echo !empty($row['id_projeto']) ? $row['id_projeto'] : "<span>Usuário em nenhum projeto.</span>" ?></td>
+            <td> 
+                <a href="#">Nova Senha</td>
+            </td>
+            <td>
+                <a href="#">Editar </a>
+            </td>
+            <td>
+                <a href="#">Deletar</a>
+            </td>
+        </tr>       
+<?php
+    } while($row=$sql_query->fetch_assoc());
+?>            
+    </table>
+<?php
+    } else {
+        echo "<p>Ainda não há usuários cadastrados.</p";
+    } 
+?>
 </body>
 </html>
