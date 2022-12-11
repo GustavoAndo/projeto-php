95744795c6b62651969615a7257a46ad8019ae78
Gustavo Ando <gustavo.k.ando@gmail.com>
2022-12-11 11:59:50 -0300

    estilizando pagina de controle de usuarios

--- index.php
+++ index.php
@@ -12,58 +12,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Usuários</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="./styles/styles.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Controle de Usuários</h1>
-<?php
-    if(isset($_SESSION['msg'])){
-        echo $_SESSION['msg'];
-        unset($_SESSION['msg']);
-    }    
-?>
-    <a href="./pages/cadastro-usuario.php">Cadastrar Usuário</a>
-<?php
-    if (!empty($row)) {
-?>
-    <table>
-        <tr>
-            <td>Nome</td>
-            <td>E-mail</td>
-            <td>Projeto</td>
-            <td colspan="3">Ação</td>
-        </tr>
-<?php
-    do{
-?>
-        <tr>
-            <td><?php echo $row['usu_nome'] ?></td>
-            <td><?php echo $row['usu_email'] ?></td>
-            <td><?php echo !empty($row['id_projeto']) ? $row['pro_nome'] : "<span>Usuário em nenhum projeto.</span>" ?></td>
-            <td> 
-                <a href="./pages/redefinicao-senha.php?matricula=<?php echo $row['usu_matricula'] ?>">Nova Senha</td>
-            </td>
-            <td>
-                <a href="./pages/edicao-usuario.php?matricula=<?php echo $row['usu_matricula'] ?>">Editar</a>
-            </td>
-            <td>
-                <a href="javascript: if(confirm('Tem certeza que deseja deletar o usuário <?php echo $row['usu_nome'] ?>?')) 
-                location.href='./services/excluir-usuario.php?matricula=<?php echo $row['usu_matricula'] ?>';">Excluir</a>
-            </td>
-        </tr>       
-<?php
-    } while($row=$sql_query->fetch_assoc());
-?>            
-    </table>
-<?php
-    } else {
-        echo "<p>Ainda não há usuários cadastrados.</p>";
-    } 
-?>
-
+    <div class="container px-5 pt-4 my-3">
+        <h1 class="text-center">Controle de Usuários</h1>
+    <?php
+        if(isset($_SESSION['msg'])){
+            echo $_SESSION['msg'];
+            unset($_SESSION['msg']);
+        }    
+    ?>
+    <div class="d-flex justify-content-center my-3">
+        <a class="btn btn-primary" href="./pages/cadastro-usuario.php">Cadastrar Usuário</a>
+    </div>
+    <?php
+        if (!empty($row)) {
+    ?>
+        <table class="mt-4 my-3 table table-striped">
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Projeto</th>
+                <th scope="col" colspan="3">Ação</th>
+            </tr>
+    <?php
+        do{
+    ?>
+            <tr>
+                <td><?php echo $row['usu_nome'] ?></td>
+                <td><?php echo $row['usu_email'] ?></td>
+                <td><?php echo !empty($row['id_projeto']) ? $row['pro_nome'] : "<span>Usuário em nenhum projeto.</span>" ?></td>
+                <td> 
+                    <a href="./pages/redefinicao-senha.php?matricula=<?php echo $row['usu_matricula'] ?>">Nova Senha</td>
+                </td>
+                <td>
+                    <a href="./pages/edicao-usuario.php?matricula=<?php echo $row['usu_matricula'] ?>">Editar</a>
+                </td>
+                <td>
+                    <a href="javascript: if(confirm('Tem certeza que deseja deletar o usuário <?php echo $row['usu_nome'] ?>?')) 
+                    location.href='./services/excluir-usuario.php?matricula=<?php echo $row['usu_matricula'] ?>';">Excluir</a>
+                </td>
+            </tr>       
+    <?php
+        } while($row=$sql_query->fetch_assoc());
+    ?>            
+        </table>
+    <?php
+        } else {
+            echo "<p>Ainda não há usuários cadastrados.</p>";
+        } 
+    ?>
+    </div>
 <script>
     function fechar() {
         document.getElementById("aviso").innerHTML = '';
     }
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
