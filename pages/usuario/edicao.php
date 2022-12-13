31d81ded0cc4ae0e40713cc6d2264e22cb3890d3
Gustavo Ando <gustavo.k.ando@gmail.com>
2022-12-13 14:28:18 -0300

    arrumado link css

--- pages/usuario/edicao.php
+++ pages/usuario/edicao.php
@@ -17,7 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Usuário</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link href="./styles/styles.css" rel="stylesheet">
+    <link href="../../styles/styles.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
