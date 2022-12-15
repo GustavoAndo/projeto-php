92ca9512cfa2034c03b509d9bcbfef9486332154
Gustavo Ando <gustavo.k.ando@gmail.com>
2022-12-15 14:10:49 -0300

    atualizando scripts de todas as paginas

--- pages/usuario/tabela.php
+++ pages/usuario/tabela.php
@@ -81,7 +81,7 @@
                     </td>
                     <td>
                         <!-- Botao que ativa o modal -->
-                        <button type="button" class="btn btn-danger btn-sm " data-toggle="modal" data-target="#modalExcluir<?php echo $row['usu_matricula'] ?>">
+                        <button type="button" class="btn btn-danger btn-sm " data-bs-toggle="modal" data-bs-target="#modalExcluir<?php echo $row['usu_matricula'] ?>">
                             Excluir
                         </button>
                         <!-- Modal -->
@@ -90,14 +90,14 @@
                             <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="modalLabel<?php echo $row['usu_matricula'] ?>">EXCLUIR USUÁRIO</h5>
-                                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
                                 </button>
                             </div>
                             <div class="modal-body">
                                 Tem certeza que deseja excluir o usuário <?php echo $row['usu_nome'] ?>?
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
                                 <a href="../../services/usuario/excluir.php?matricula=<?php echo $row['usu_matricula']?>" class="btn btn-danger">Excluir</a>
                             </div>
                             </div>
@@ -117,7 +117,7 @@
         </div>
     </section>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 </body>
 </html>
